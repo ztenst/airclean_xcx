3714df58c846eda16001a95b175857440ff2041b
sysweet <sysweet@hotmail.com>
2018-11-20 11:03:43 +0800

    11

--- pages/product/select.js
+++ pages/product/select.js
@@ -7,7 +7,17 @@ Page({
     this.Global.pubsub.emit('product-category',dataset);
     wx.navigateBack();
   },
+  init : function() {
+    this.Api.getTagArr().then(obj=>{
+      console.log(obj);
+      this.setData({
+        list : obj
+      });
+    })
+  },
   onLoad: function(options){
     this.Global = app.Global;
+    this.Api = this.Global.Api;
+    this.init();
   }
 })
